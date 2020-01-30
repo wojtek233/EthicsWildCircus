2c0b5853d53978a74d0352aaf37b763e2be0f3ea
Wojtek <wojtek.zdebski@hotmail.fr>
2020-01-30 23:30:59 +0100

    inst carousel

--- front/src/component/DisplayPerf.js
+++ front/src/component/DisplayPerf.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {Carousel} from 'react-responsive-carousel';
+
 
 function DisplayPerf(props){
 
@@ -9,13 +12,18 @@ function DisplayPerf(props){
       <Link className='arrowBackLink' to="/">&lsaquo;</Link>
     </button>
     <h2>Performances</h2>
-    {props.perfs.map((perf) => 
-      <div>
-      <img src={`http://localhost:8000/${perf.photo}`} alt={'profil perf'} />
-      <h2>{perf.name}</h2>
-      <p>{perf.description}</p>
-      </div>
-      )}
+    <Carousel autoPlay width="60%" dynamicHeight>
+              {props.perfs.map((perf) => 
+              <div>
+              <img src={`http://localhost:8000/${perf.photo}`} alt={'profil perf'} />
+                <div className="legend">
+                <h2 >{perf.name} <br/>
+                {perf.description}</h2>
+              </div>
+              </div>
+            )}
+      </Carousel>
+
       <Link to={{pathname:"/formperf"     
               }}>
                 <button>Create a performance</button>
